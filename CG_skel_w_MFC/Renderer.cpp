3374c252f19f87b24fe4b2e876a028896bc3d5a0
ogurdima@gmail.com <ogurdima@gmail.com@dfdac2ee-e722-7838-2c2b-fc8c782fd078>
2013-01-14 19:38:40 +0000

    OpenGL, baby!

--- CG_skel_w_MFC/Renderer.cpp
+++ CG_skel_w_MFC/Renderer.cpp
@@ -4,7 +4,8 @@
 
 Renderer::Renderer(int _w, int _h) :
 deviceW(_w),
-deviceH(_h)
+deviceH(_h),
+program(-1)
 {
 	cout << "Renderer::Renderer: " << deviceW << "x" << deviceH << endl;
 }
@@ -18,10 +19,88 @@ Renderer::~Renderer(void)
 void Renderer::SetShadingProgram(GLuint _program)
 {
 	program = _program;
+	glUseProgram(program);
 }
 
-//bool Renderer::InitModel(MeshModel m)
-//{
-//	GLuint vao;
-//	return false;
-//}
+ModelBind Renderer::BindModel(vector<vec4> pts, vector<vec4> normals /*also textures*/)
+{
+	if (program < 0)
+	{
+		throw std::exception("Binding model without program set");
+	}
+
+	GLuint buffers[1];
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glGenBuffers(1, buffers);
+
+	GLuint vPositionLoc = glGetAttribLocation(program, "vPosition");
+	GLuint modelLoc = glGetUniformLocation(program, "model");
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec4) * pts.size(), &pts[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(vPositionLoc);
+	glVertexAttribPointer(vPositionLoc, 4, GL_FLOAT, 0, 0, 0);
+
+	ModelBind b;
+	b.modelHnd = modelLoc;
+	b.ptsHnd = vPositionLoc;
+	b.vao = vao;
+	return b;
+}
+
+CameraBind Renderer::BindCamera()
+{
+	if (program < 0)
+	{
+		throw std::exception("Binding camera without program set");
+	}
+
+	GLuint viewLoc = glGetUniformLocation(program, "view");
+	GLuint projectLoc = glGetUniformLocation(program, "projection");
+
+	CameraBind b;
+	b.projectHnd = projectLoc;
+	b.viewHnd = viewLoc;
+	return b;
+}
+
+void Renderer::SetUniformMatrices(vector<GLuint> handles, vector<mat4> values)
+{
+	if (program < 0 || handles.size() != values.size() || handles.size() == 0)
+	{
+		throw std::exception("Bad arguments in SetUniformMatrices");
+	}
+	for (int i = 0; i < handles.size(); i++)
+	{
+		glUniformMatrix4fv(handles[i], 1, GL_TRUE, values[i]);
+	}
+}
+
+void Renderer::SetUniformMatrix(GLuint handle, mat4 val)
+{
+	if (program < 0)
+	{
+		throw std::exception("Bad arguments in SetUniformMatrix");
+	}
+	glUniformMatrix4fv(handle, 1, GL_TRUE, val);
+}
+
+void Renderer::DrawTriangles(GLuint vao, int count)
+{
+	if (program < 0)
+	{
+		throw std::exception("DrawTriangles called with not program set");
+	}
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, count);
+}
+
+void Renderer::SwapBuffers()
+{
+	glutSwapBuffers();
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+}
--- CG_skel_w_MFC/Shaders/flat_vshader.glsl
+++ CG_skel_w_MFC/Shaders/flat_vshader.glsl
@@ -1,15 +1,17 @@
 #version 150 
 
 in vec4 vPosition;
-in vec4 vColor;
+//in vec4 vNormal;
+//in vec4 vColor;
 
-uniform mat4 model_view;
+uniform mat4 model;
+uniform mat4 view;
 uniform mat4 projection;
 
 out vec4 color;
 
 void main()
 {
-	gl_Position = projection * model_view * vPosition;
-	color = vColor;
+	gl_Position = projection * view * model * vPosition;
+	color = vec4(1,0,0,0);
 }
